7abdd7709313a9096df276af40518987d26c2ed2
Alecx Moritz <alecxmoritz7@gmail.com>
2019-02-12 12:03:19 -0500

    dislike controller also increments shade's dislikes

--- controllers/dislikecontroller.js
+++ controllers/dislikecontroller.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const Dislike = require('../db').import('../models/dislike');
 const validateSession = require('../middleware/validate-session');
+const Shade = require('../db').import('../models/shade');
 
 // get dislikes by user id
 router.get('/user/:id', (req, res) => {
@@ -9,16 +10,16 @@ router.get('/user/:id', (req, res) => {
             userId: req.params.id
         }
     })
-    .then(
-        findSuccess = shades => {
-            res.status(200).json(shades);
-        },
+        .then(
+            findSuccess = shades => {
+                res.status(200).json(shades);
+            },
 
-        findFail = err => {
-            console.log(err.message);
-            res.status(500).send(err.message);
-        }
-    )
+            findFail = err => {
+                console.log(err.message);
+                res.status(500).send(err.message);
+            }
+        )
 
 });
 
@@ -40,20 +41,41 @@ router.get('/:id', validateSession, (req, res) => {
 
 // add dislike
 router.post('/:id', validateSession, (req, res) => {
-    Dislike.create({
-        userId: req.user.id,
-        shade_id: req.params.id
-    })
-    .then(
-        dislikeSuccess = dislike => {
-            res.status(200).json(dislike);
-        },
+    Shade.findById(req.params.id)
+        .then(
+            foundShadeSuccess = shade => {
+                let newShadeDislikes = shade.dislikes + 1;
+                Shade.update({
+                    dislikes: newShadeDislikes
+                },
+                    {
+                        where: {
+                            id: req.params.id
+                        }
+                    })
+                    .then(
+                        Dislike.create({
+                            userId: req.user.id,
+                            shade_id: req.params.id
+                        })
+                            .then(
+                                dislikeSuccess = dislike => {
+                                    res.status(200).json(dislike);
+                                },
 
-        dislikeFail = err => {
-            console.log(err.message);
-            res.status(500).send(err.message);
-        }
-    )
+                                dislikeFail = err => {
+                                    console.log(err.message);
+                                    res.status(500).send(err.message);
+                                }
+                            )
+                    )
+            },
+
+            foundShadeFailed = err => {
+                console.log(err.message);
+                res.status(500).send(err.message);
+            }
+        )
 });
 
 // remove dislike
@@ -64,17 +86,17 @@ router.delete('/:id', validateSession, (req, res) => {
             userId: req.user.id
         }
     })
-    .then(
-        deleteSuccess = recordsChanged => {
-            res.status(200).json(recordsChanged);
-        },
+        .then(
+            deleteSuccess = recordsChanged => {
+                res.status(200).json(recordsChanged);
+            },
 
-        deleteFail = err => {
-            console.log(err.message);
-            res.status(500).send(err.message);
-        }
-    
-    )
+            deleteFail = err => {
+                console.log(err.message);
+                res.status(500).send(err.message);
+            }
+
+        )
 });
 
 module.exports = router;
